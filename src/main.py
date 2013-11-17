7facebb771a19dce6071f9291121613ecc5a9eb6
Sai Cheemalapati <saicheems@gmail.com>
2013-11-17 00:08:27 +0000

    added basic serial coms

--- src/main.py
+++ src/main.py
@@ -1,8 +1,30 @@
 from sklearn import svm
 
-def main():
+import serial
+import sys
 
-	end
+DEVICE = '/dev/ttyACM0'
+
+def list_from_string(input):
+	"""Reads in a comma delmited string and returns a list."""
+	output = map(int, input.split(','))
+	return output
+
+def read_from_serial(ser):
+	input = ser.readline()
+	# read input string from serial in here
+
+	return input
+
+def main():
+	ser = serial.Serial(DEVICE, 9600)
+
+	# read input from serial
+	serial_input = read_from_serial(ser)
+	# print comma delimited list from input string
+	print list_from_string(serial_input)
+
+	
 
 if __name__ == "__main__":
 	main()
---
+++ src/serialin.py
@@ -0,0 +1,7 @@
+"""Test reading in data from serial in"""
+import serial
+ser = serial.Serial('/dev/ttyACM0', 9600)
+
+while 1 :
+    ser.readline()
+
